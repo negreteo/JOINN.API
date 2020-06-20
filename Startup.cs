c3a3f05bdcb735254ebebb6e11c8bd4f2531f914
Oscar Negrete <oscar.negrete09@gmail.com>
2020-06-20 12:44:55 -0400

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,60 @@
+using JOINN.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JOINN.API
+{
+   public class Startup
+   {
+      public Startup (IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
+
+      public IConfiguration Configuration { get; }
+
+      // This method gets called by the runtime. Use this method to add services to the container.
+      public void ConfigureServices (IServiceCollection services)
+      {
+         services.AddControllers ();
+
+         services.AddDbContext<JOINNDbContext> (opts =>
+         {
+            opts.EnableDetailedErrors ();
+            opts.UseSqlServer (Configuration.GetConnectionString ("joinn.dev"));
+         });
+      }
+
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         if (env.IsDevelopment ())
+         {
+            app.UseDeveloperExceptionPage ();
+         }
+
+         //app.UseHttpsRedirection ();
+         app.UseRouting ();
+
+         // Place before UserRouting and before UseAuthorization
+         app.UseCors (builder => builder
+            .WithOrigins (
+               "http://localhost:8080"
+            )
+            .AllowAnyMethod ()
+            .AllowAnyHeader ()
+         );
+
+         //app.UseAuthorization();
+
+         app.UseEndpoints (endpoints =>
+         {
+            endpoints.MapControllers ();
+         });
+      }
+   }
+}
